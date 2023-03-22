2f054964e7df7ca318187f7a062187d364c16f56
Jan <jan@janMacBookPro.local>
2023-03-22 13:42:59 +0800

    casbin enforcer

---
+++ internal/service/sso.go
@@ -0,0 +1,19 @@
+package service
+
+import (
+	"context"
+
+	pb "sso/api/sso/v1"
+)
+
+type SSOService struct {
+	pb.UnimplementedSSOServer
+}
+
+func NewSSOService() *SSOService {
+	return &SSOService{}
+}
+
+func (s *SSOService) UserList(ctx context.Context, req *pb.UserListRequest) (*pb.UserListReply, error) {
+	return &pb.UserListReply{}, nil
+}
